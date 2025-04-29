8f880d088cb7e58af8d6a2f3054cc77b1659a58c
Philip Stampe <philipstampe@gmail.com>
2025-04-29 16:26:05 +0200

    Only return publicuser

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,10 +1,12 @@
 import { users } from "./data/users";
 import { PublicUser } from "./types";
 
-export function getPublicUserById(id: number): PublicUser | null {
+export function getPublicUserById(id: number): PublicUser {
   const user = users.find((user) => user.id === id);
 
-  if (!user) return null;
+  if (!user) {
+    throw new Error("User not found");
+  }
 
   return {
     id: user.id,
